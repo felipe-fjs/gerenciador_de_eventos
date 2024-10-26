1ea272f0f65ab326ad0cb13cd4938ca3b2962c11
Felipe Joaquim <felipejoaquimtech@gmail.com>
2024-10-25 22:54:58 -0300

    concluindo (eu acho) rota /sub_event/editar

--- app/controllers/sub_event.py
+++ app/controllers/sub_event.py
@@ -34,7 +34,7 @@ def sub_event_home(event_id, subevent_id):
     except SQLAlchemyError:
         flash("ocorreu um erro ao acessar o sub evento...")
 
-    return render_template('sub_event/info.html', sub_event=sub_event)
+    return render_template('sub_event/info.html', sub_event=sub_event, event_id=event_id)
 
 
 @sub_event_route.route('/<event_id>/novo', methods=['GET','POST'])
@@ -66,13 +66,52 @@ def sub_event_new(event_id):
     return render_template("sub_event/admin/new.html")
 
 
-@sub_event_route.route('/<event_id>/<subevent_id>/editar')
+@sub_event_route.route('/<event_id>/<subevent_id>/editar', method=['GET', 'PUT'])
+@login_required
 @admin_or_above
-def sub_event_edit(subevent_id):
-    pass
+def sub_event_edit(event_id, subevent_id):
+    if request.method == 'PUT':
+        try:
+            sub_event = SubEvent.query.filter_by(id=subevent_id).first()
+            
+            if SubEvent.query.filter_by(slug=request.form.get('slug')).first():
+                if not sub_event.slug != request.form.get('slug'):
+                    flash('Essa slug já está registrada!')
+                    # Essa template só será utilizada quando um erro ocorreu ou uma slug já existir (talvez criar uma api no futuro pra verificar isso já no formulário)
+                    return render_template('sub_event/admin/erro_edit.html', sub_event=sub_event)
+            
+            sub_event.title = request.fom.get('title')
+            sub_event.desc = request.fom.get('desc')
+            sub_event.slug = request.fom.get('slug')
+            sub_event.start = request.fom.get('start')
+            sub_event.end = request.fom.get('end')
+            sub_event.classroom = request.fom.get('classroom')
+
+            db.session.commit()
+            
+        except SQLAlchemyError:
+            flash(f'Ocorreu um erro ao tentar atualizar as informações do sub_evento')
+            return render_template('sub_event/admin/erro_new.html', sub_event=sub_event)
+        
+        flash("Atualizações salvas com sucesso!")
+        return redirect(url_for("sub_event.sub_event_home", event_id=event_id, subevent_id=subevent_id))
+
+    try:
+        if not SubEvent.query.filter_by(id=subevent_id, event_id=event_id).first():
+            flash("Sub evento não encontrado!")
+            return redirect(url_for('event.event_home', event_id=event_id))
+        
+        sub_event = SubEvent.query.filter_by(id=subevent_id, event_id=event_id).first()
+
+    except SQLAlchemyError:
+        flash("Ocorreu um erro ao acessar as informações do sub evento!")
+        return redirect(url_for('event.event_home', event_id=event_id))
+
+    return render_template("sub_event/admin/new.html", sub_event=sub_event)
 
 
 @sub_event_route.route('/<event_id>/<subevent_id>/delete')
+@login_required
 @admin_or_above
 def sub_event_delete(subevent_id):
     pass
